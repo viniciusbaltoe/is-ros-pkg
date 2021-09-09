86e37fb90ad9aab4a4c7cd749c3f757ac250cfd6
vinicius <viniciusbaltoe@gmail.com>
2021-09-09 19:57:24 -0300

    Correcting config.json path at is_ros_gt.py

--- is_ros_pkg/nodes/is_ros_gt.py
+++ is_ros_pkg/nodes/is_ros_gt.py
@@ -2,11 +2,13 @@
 from ros_driver.driver import ROS_Robot
 from gateway.gateway import RobotGateway
 from is_msgs.common_pb2 import Position
+import os.path
 import json
 import sys
 
 def main():
-    config_file = sys.argv[1] if len(sys.argv) > 1 else '../etc/conf/config.json'
+    homedir = os.path.expanduser("~")
+    config_file = sys.argv[1] if len(sys.argv) > 1 else '{}/catkin_ws/src/is-ros-pkg/is_ros_pkg/etc/conf/config.json'.format(homedir)
     config = json.load(open(config_file, 'r'))
 
     broker_uri = config['broker_uri']
